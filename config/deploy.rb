3631a8bbc5dea8600ac5cd46ae8f82a4af8d6b31
root <root@drupal7.(none)>
2014-10-15 00:26:05 +0000

    add current to app path

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :branch, "master"
 server "192.168.1.160", :app, :web, :db 
 #role :app, "192.168.1.160"
 
-set :app_path, "/var/www/drupal7"
+set :app_path, "/var/www/drupal7/current"
 set :shared_children, ['sites/default/files']
 set :shared_files, ['sites/default/settings.php']
 set :deploy_to, "/var/www/drupal7"
